b65227739c2870f1b3aa3023556de086d6f1787b
XeeMpee <xempee@gmail.com>
2019-02-19 16:56:49 +0100

    Generate profile list done.

--- darkestDungeonSaves/Controller.py
+++ darkestDungeonSaves/Controller.py
@@ -1,3 +1,6 @@
+import os
+import re
+
 class Controller:
     """ 
     Class proposed to controll mechanic of application
@@ -5,14 +8,26 @@ class Controller:
 
     def __init__(self):
         
+        self.__pathToSaves = '/home/xeempee/.local/share/Steam/userdata/109096097/262060/remote/'
+        self.__profiles = list()
         self.__orderByOptions = [
             "Name",
             "Date ascending",
             "Date descending"
         ]
 
+
     def getOrderOptions(self):
         return self.__orderByOptions
-    
 
+
+    def generateProfilesList(self):
+        listOfProfiles = os.listdir(self.__pathToSaves)
+        toSave = list()
+        
+        for i in listOfProfiles:
+            if(re.search('^profile_*',i)):
+                toSave.append(i)
+    
+        self.__profiles = toSave
     
--- darkestDungeonSaves/main.py
+++ darkestDungeonSaves/main.py
@@ -1,6 +1,11 @@
 from MainWindow import *
+from Controller import *
 
 if __name__ == "__main__":
+
+    controller = Controller()
+    controller.generateProfilesList()
+    
     mainWindow = MainWindow()
     mainWindow.run()
     pass
